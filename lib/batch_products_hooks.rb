c2934b285999e1b473a5f5151c42a231b3c2e6f2
Minus <minustehbare@gmail.com>
2011-05-02 00:54:53 -0400

    clean up

--- lib/batch_products_hooks.rb
+++
@@ -1,6 +0,0 @@
-class BatchProductsHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
-  insert_after :admin_product_sub_tabs do
-    %(<%= tab(:product_datasheets, :label => :batch_updates) %>)
-  end
-end
