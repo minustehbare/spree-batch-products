9086b67a9050b2ec297435ddb4c7fd4ddc65c33b
Minus <minustehbare@gmail.com>
2011-06-21 14:13:50 -0400

    updating for 0-70-x Spree

--- lib/spree_batch_products_hooks.rb
+++ lib/spree_batch_products_hooks.rb
@@ -1,6 +1,8 @@
 class BatchProductsHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :admin_product_sub_tabs do
-    %(<%= tab(:product_datasheets, :label => :batch_updates) %>)
-  end
+  Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
+                     :name => "batch_products_admin_product_sub_tabs",
+                     :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+                     :text => "<%= tab(:product_datasheets, :label => :batch_updates) %>",
+                     :disabled => false)
 end
