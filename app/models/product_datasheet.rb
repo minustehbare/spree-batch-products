df885810da9a9bc3d9cd243daa816e79b8d3fcd3
Minus <minustehbare@gmail.com>
2011-09-22 10:58:08 -0400

    refactoring product_datasheet to pass tests

--- app/models/product_datasheet.rb
+++ app/models/product_datasheet.rb
@@ -1,6 +1,13 @@
 class ProductDatasheet < ActiveRecord::Base
   require 'spreadsheet'
   
+  attr_accessor :queries_failed, :records_failed, :records_matched, :records_updated
+  
+  before_save :update_statistics
+  
+  after_find :setup_statistics
+  after_initialize :setup_statistics
+  
   has_attached_file :xls, :path => ":rails_root/uploads/product_datasheets/:id/:basename.:extension"  
   
   validates_attachment_presence :xls
@@ -20,14 +27,16 @@ class ProductDatasheet < ActiveRecord::Base
   # Iterates row-by-row to populate a hash of { :attribute => :value } pairs, uses this hash to create or update records accordingly
   ####################
   def perform
-    workbook = Spreadsheet.open self.xls.to_file
+    workbook =
+    begin
+      Spreadsheet.open self.xls.to_file
+    rescue
+      puts 'Failed to open xls attachment for processing'
+      return false
+    end
     worksheet = workbook.worksheet(0)
     columns = [worksheet.dimensions[2], worksheet.dimensions[3]]
     headers = worksheet.row(0)
-    @records_matched = 0
-    @records_updated = 0
-    @records_failed = 0
-    @failed_queries = 0
     
     ####################
     # Creating Variants:
@@ -50,38 +59,36 @@ class ProductDatasheet < ActiveRecord::Base
       for i in columns[0]..columns[1]
         attr_hash[headers[i]] = row[i] unless row[i].nil?
       end
-      puts headers.to_s
       if headers[0] == 'id' and row[0].nil? and headers.include? 'product_id'
         create_variant(attr_hash)
       elsif headers[0] == 'id' and row[0].nil?
         create_product(attr_hash)
       elsif Product.column_names.include?(headers[0])
-        process_products(headers[0], row[0], attr_hash)
+        update_products(headers[0], row[0], attr_hash)
       elsif Variant.column_names.include?(headers[0])
-        process_variants(headers[0], row[0], attr_hash)
+        update_variants(headers[0], row[0], attr_hash)
       else
-        @failed_queries = @failed_queries + 1
+        @queries_failed = @queries_failed + 1
       end
     end
-    attr_hash = { :processed_at => Time.now, 
-                  :matched_records => @records_matched, 
-                  :failed_records => @records_failed, 
-                  :updated_records => @records_updated, 
-                  :failed_queries => @failed_queries }
-    self.update_attributes(attr_hash)
+    self.update_attribute(:processed_at => Time.now)
   end
   
   def create_product(attr_hash)
     new_product = Product.new(attr_hash)
-    @failed_queries = @failed_queries + 1 if not new_product.save
+    @queries_failed = @queries_failed + 1 if not new_product.save
   end
   
   def create_variant(attr_hash)
     new_variant = Variant.new(attr_hash)
-    @failed_queries = @failed_queries + 1 if not new_variant.save
+    begin
+      new_variant.save
+    rescue
+      @queries_failed = @queries_failed + 1
+    end
   end
   
-  def process_products(key, value, attr_hash)
+  def update_products(key, value, attr_hash)
     products_to_update = Product.where(key => value).all
     @records_matched = @records_matched + products_to_update.size
     products_to_update.each { |product| 
@@ -90,10 +97,10 @@ class ProductDatasheet < ActiveRecord::Base
                                         else
                                           @records_failed = @records_failed + 1
                                         end }
-    @failed_queries = @failed_queries + 1 if products_to_update.size == 0
+    @queries_failed = @queries_failed + 1 if products_to_update.size == 0
   end
   
-  def process_variants(key, value, attr_hash)
+  def update_variants(key, value, attr_hash)
     variants_to_update = Variant.where(key => value).all
     @records_matched = @records_matched + variants_to_update.size
     variants_to_update.each { |variant| 
@@ -102,7 +109,21 @@ class ProductDatasheet < ActiveRecord::Base
                                         else
                                           @records_failed = @records_failed + 1
                                         end }
-    @failed_queries = @failed_queries + 1 if variants_to_update.size == 0
+    @queries_failed = @queries_failed + 1 if variants_to_update.size == 0
+  end
+  
+  def update_statistics
+    self.matched_records = @records_matched
+    self.failed_records = @records_failed
+    self.updated_records = @records_updated
+    self.failed_queries = @queries_failed
+  end
+  
+  def setup_statistics
+    @queries_failed = 0
+    @records_failed = 0
+    @records_matched = 0
+    @records_updated = 0
   end
   
   def processed?
